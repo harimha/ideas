0b001afafe83140def2454752ea77b1b623afec5
Harim Jeong <cceeddcc@naver.com>
2023-07-27 17:31:03 +0900

    developing

--- analysis/strategy/base.py
+++ analysis/strategy/base.py
@@ -1,28 +1,79 @@
-class Parent1:
-    def __init__(self):
-        self.var1 = "Parent1"
+import fdata.api as api
+from pandas.api.types import is_list_like
+import plotly.graph_objects as go
+from datetime import datetime
 
-    def method1(self):
-        print("Method 1 from Parent1")
+class Analysis():
+    def _set_rawdata(self, df, column):
+        raw_data = df[[column]].copy()
+        raw_data.columns = ["value"]
 
+        return raw_data
 
-class Parent2:
-    def __init__(self):
-        self.var2 = "Parent2"
+class Indicators(Analysis):
+    def sma(self, df, column, windows):
+        df_indi = self._set_rawdata(df, column)
+        if is_list_like(windows):
+            for window in windows:
+                df_indi[f"sma{window}"] = df_indi["value"].rolling(window).mean()
+        else:
+            df_indi[f"sma{windows}"] = df_indi["value"].rolling(windows).mean()
 
-    def method2(self):
-        print("Method 2 from Parent2")
-
-
-class Child(Parent1, Parent2):
-    def __init__(self):
-        # Parent1의 __init__ 메서드 호출
-        Parent1.__init__(self)
-        Parent2.__init__(self)
-
-    def method3(self):
-        print("Method 3 from Child")
+        return df_indi
 
 
-child = Child()
-print(child.var2)  # Output: Parent1
+
+class Visualization():
+    def _fig_show(self, fig, name=None, html=True):
+        if html:
+            now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            fig.write_html(f"{name}_{now}.html")
+        else:
+            fig.show()
+
+    def visualize(self, df, columns:str or [], mode="markers", name=None, html=True):
+        fig = go.Figure()
+        if is_list_like(columns):
+            for col in columns:
+                fig.add_trace(go.Scatter(x=df.index,
+                                         y=df[col],
+                                         mode=mode))
+        else:
+            fig.add_trace(go.Scatter(x=df.index,
+                                     y=df[col],
+                                     mode=mode))
+        self._fig_show(fig, name, html)
+
+
+df = api.stock_c("삼성전자", "20200101", "20230101")
+indi = Indicators()
+df_indi = indi.sma(df,"삼성전자", [20,60])
+vis =Visualization()
+vis.visualize(df_indi, df_indi.columns, name="삼성전자", mode="lines+markers")
+
+class SubIndicator():
+
+    def set_sub_indicators(self):
+        sub_indicators
+
+    def add_sub_indicators(self, df, col_name):
+        df_sub = df.copy()
+        df_sub[col_name] = df
+
+        return df_sub
+    
+    def add_sub_indicators(self, df, column):
+        df_sub = df[[column]].copy()
+        df_sub.columns = ["value"]
+        df_sub["midline"] = df_sub[self.column].rolling(20).mean()
+        df_sub["upper_bound"] = \
+            df_sub[self.column].rolling(20).mean() + 2 * df_sub[self.column].rolling(20).std()
+        df_sub["lower_bound"] = \
+            df_sub[self.column].rolling(20).mean() - 2 * df_sub[self.column].rolling(20).std()
+
+        return df_sub
+    
+
+
+class Algorithm():
+    pass
